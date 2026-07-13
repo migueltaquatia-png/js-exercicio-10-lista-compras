4106a1a86637d54193ea751ed9d0255163cc19bc
miguel rosa taquatia <miguelito23rosa@gmail.com>
2026-07-13 14:08:25 -0300

    atividade 10

---
+++ atividade10.js
@@ -0,0 +1,6 @@
+let compras = ["arroz", "feijão", "macarrão", "leite", "café"];
+
+console.log("Lista de compras:");
+for (let i = 0; i < compras.length; i++) {
+    console.log((i + 1) + " - " + compras[i]);
+}
